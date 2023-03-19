37ba5a780f9ede454ae7085bd1cf579262a97b18
viveeeeeek <viveksonawane600@gmail.com>
2023-03-19 10:44:47 +0530

    Added RefreshIndicator

--- lib/device/mobile/homepage.mobile.dart
+++ lib/device/mobile/homepage.mobile.dart
@@ -37,67 +37,72 @@ class _MobileHomepageState extends State<MobileHomepage> {
       child: Scaffold(
         body: Stack(
           children: [
-            ListView(
-              controller: controller,
-              dragStartBehavior: DragStartBehavior.down,
-              physics: const ClampingScrollPhysics(),
-              children: [
-                Container(
-                  //! Main background colour of home
-                  color: const Color(0xff050505),
-                  child: Padding(
-                    padding: screenSize.width < 500
-                        ? const EdgeInsets.all(25.0)
-                        : const EdgeInsets.all(75.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        sizedBox150,
-                        AutoScrollTag(
-                            key: const ValueKey(1),
-                            index: 1,
-                            controller: controller,
-                            child: const Header()),
-                        const SizedBox(
-                          height: 250,
-                        ),
-                        Center(
-                          child: InkWell(
-                            onTap: () {
-                              controller.scrollToIndex(2);
-                            },
-                            child: HoverWidget(
-                              child: Lottie.asset("assets/arrow-down.json",
-                                  height: 25, width: 25),
+            RefreshIndicator(
+              onRefresh:
+                  _refreshData, // specify the function to call when the user pulls down
+
+              child: ListView(
+                controller: controller,
+                dragStartBehavior: DragStartBehavior.down,
+                physics: const ClampingScrollPhysics(),
+                children: [
+                  Container(
+                    //! Main background colour of home
+                    color: const Color(0xff050505),
+                    child: Padding(
+                      padding: screenSize.width < 500
+                          ? const EdgeInsets.all(25.0)
+                          : const EdgeInsets.all(75.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          sizedBox150,
+                          AutoScrollTag(
+                              key: const ValueKey(1),
+                              index: 1,
+                              controller: controller,
+                              child: const Header()),
+                          const SizedBox(
+                            height: 250,
+                          ),
+                          Center(
+                            child: InkWell(
+                              onTap: () {
+                                controller.scrollToIndex(2);
+                              },
+                              child: HoverWidget(
+                                child: Lottie.asset("assets/arrow-down.json",
+                                    height: 25, width: 25),
+                              ),
                             ),
                           ),
-                        ),
-                        const SizedBox(
-                          height: 250,
-                        ),
-                        AutoScrollTag(
-                            key: const ValueKey(2),
-                            index: 2,
-                            controller: controller,
-                            child: const About()),
-                        sizedBox100,
-                        const Skills(),
-                        sizedBox150,
-                        const MobileBlogs(),
-                        sizedBox150,
-                        // // Work(),
-                        AutoScrollTag(
-                            key: const ValueKey(4),
-                            index: 4,
-                            controller: controller,
-                            child: const ContactCard()),
-                        sizedBox150,
-                        const Footer(),
-                      ],
+                          const SizedBox(
+                            height: 250,
+                          ),
+                          AutoScrollTag(
+                              key: const ValueKey(2),
+                              index: 2,
+                              controller: controller,
+                              child: const About()),
+                          sizedBox100,
+                          const Skills(),
+                          sizedBox150,
+                          const MobileBlogs(),
+                          sizedBox150,
+                          // // Work(),
+                          AutoScrollTag(
+                              key: const ValueKey(4),
+                              index: 4,
+                              controller: controller,
+                              child: const ContactCard()),
+                          sizedBox150,
+                          const Footer(),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ],
         ),
